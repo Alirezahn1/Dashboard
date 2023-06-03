a917fa0e87f546bee0dba601e3de7035bb1c30e5
alireza <alirezahn1@yahoo.com>
2023-06-03 13:31:05 +0330

    feat log out view and url

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,4 +1,4 @@
-from .views import RegistrationView, UsernameValidationView, EmailValidationView, LoginView
+from .views import RegistrationView, UsernameValidationView, EmailValidationView, LoginView, LogoutView
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt,csrf_protect
 
@@ -11,4 +11,5 @@ urlpatterns = [
     # path('activate/<uidb64>/<token>',
     #      VerificationView.as_view(), name='activate'),
     path('login', LoginView.as_view(), name="login"),
+    path('logout', LogoutView.as_view(), name="logout"),
 ]
