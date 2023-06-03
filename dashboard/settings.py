15becc108006a2064e97775aefa6e2190fdad6d0
alireza <alirezahn1@yahoo.com>
2023-06-03 12:20:57 +0330

    feat send email setting

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -140,3 +140,10 @@ django_heroku.settings(locals())
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+EMAIL_PORT = 587
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
