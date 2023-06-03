8ff20ae1621d2f9666a367c214a979c6f78623ae
alireza <alirezahn1@yahoo.com>
2023-06-03 10:30:17 +0330

    feat email validation view

--- authentication/views.py
+++ authentication/views.py
@@ -1,12 +1,15 @@
 import json
-
 from django.contrib import messages
+from django.core.validators import validate_email
 from django.http import JsonResponse
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
 from django.views import View
 from django.contrib.auth.models import User
 from django.contrib.sites.shortcuts import get_current_site
 from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+
 from .utils import account_activation_token
 from django.utils.encoding import force_bytes, DjangoUnicodeDecodeError
 from django.core.mail import EmailMessage
@@ -77,3 +80,13 @@ class UsernameValidationView(View):
         if User.objects.filter(username=username).exists():
             return JsonResponse({'username_error': 'sorry username in use,choose another one '}, status=409)
         return JsonResponse({'username_valid': True})
+
+class EmailValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        email = data['email']
+        if not validate_email(email):
+            return JsonResponse({'email_error': 'Email is invalid'}, status=400)
+        if User.objects.filter(email=email).exists():
+            return JsonResponse({'email_error': 'sorry email in use,choose another one '}, status=409)
+        return JsonResponse({'email_valid': True})
