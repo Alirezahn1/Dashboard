57fbc87e4d235fde4d87cf1533fedfcd55c7098a
alireza <alirezahn1@yahoo.com>
2023-06-04 13:22:15 +0330

    feat category  model

--- expenses/models.py
+++ expenses/models.py
@@ -16,3 +16,12 @@ class Expense(models.Model):
 
     class Meta:
         ordering: ['-date']
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.nam
