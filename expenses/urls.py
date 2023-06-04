6c4d98d1e215e88efa0df37187f00c4e010e33cd
alireza <alirezahn1@yahoo.com>
2023-06-04 13:53:18 +0330

    fix add expense url

--- expenses/urls.py
+++ expenses/urls.py
@@ -4,7 +4,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
- path('',views.index,name='expenses')
+ path('',views.index,name='expenses'),
+ path('add-expense', views.add_expense, name="add-expenses"),
 
 
 ]
