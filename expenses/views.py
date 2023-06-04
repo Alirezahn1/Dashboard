32afa4a2c8533afdb807f52a2b1bbfd2516acf3e
alireza <alirezahn1@yahoo.com>
2023-06-04 13:50:16 +0330

    feat add expense view

--- expenses/views.py
+++ expenses/views.py
@@ -1,6 +1,7 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from expenses.models import Expense, Category
 from userpreferences.models import UserPreference
@@ -20,3 +21,32 @@ def index(request):
         'currency': currency
     }
     return render(request, 'expenses/index.html', context)
+
+def add_expense(request):
+    categories = Category.objects.all()
+    context = {
+        'categories': categories,
+        'values': request.POST
+    }
+    if request.method == 'GET':
+        return render(request, 'expenses/add_expense.html', context)
+
+    if request.method == 'POST':
+        amount = request.POST['amount']
+
+        if not amount:
+            messages.error(request, 'Amount is required')
+            return render(request, 'expenses/add_expense.html', context)
+        description = request.POST['description']
+        date = request.POST['expense_date']
+        category = request.POST['category']
+
+        if not description:
+            messages.error(request, 'description is required')
+            return render(request, 'expenses/add_expense.html', context)
+
+        Expense.objects.create(owner=request.user, amount=amount, date=date,
+                               category=category, description=description)
+        messages.success(request, 'Expense saved successfully')
+
+        return redirect('expenses')
