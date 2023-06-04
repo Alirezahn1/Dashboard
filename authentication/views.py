7e7ee16124bc934b71b65a2a012295cfbddd1cd2
alireza <alirezahn1@yahoo.com>
2023-06-04 14:16:05 +0330

    fix login view

--- authentication/views.py
+++ authentication/views.py
@@ -128,7 +128,7 @@ class LoginView(View):
                     auth.login(request, user)
                     messages.success(request, 'Welcome, ' +
                                      user.username+' you are now logged in')
-                    return redirect('index')
+                    return redirect('expenses')
                 # messages.error(
                 #     request, 'Account is not active,please check your email')
                 # return render(request, 'authentication/login.html')
