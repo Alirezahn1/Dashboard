72ff8b5835ba46e879feca4b8116f3ac2e6544f4
alireza <alirezahn1@yahoo.com>
2023-06-04 16:15:12 +0330

    feat add income view and page and url

--- incomes/urls.py
+++ incomes/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
  path('', views.index, name="income"),
- # path('add-expense', views.add_expense, name="add-expenses"),
+ path('add-expense', views.add_expense, name="add-expenses"),
  # path('edit-expense/<int:id>', views.expense_edit, name="expense-edit"),
  # path('expense-delete/<int:id>', views.delete_expense, name="expense-delete"),
  # path('search-expenses', csrf_exempt(views.search_expenses),
