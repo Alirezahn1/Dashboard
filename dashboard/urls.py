f9b0c4eecde816176f85a21a65befd937fc91e41
alireza <alirezahn1@yahoo.com>
2023-06-04 12:59:21 +0330

    feat Preference urls

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('expenses.urls')),
     path('', include('authentication.urls')),
+    path('',include('userpreferences.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
