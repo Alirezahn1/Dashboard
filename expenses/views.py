3926a585e02e13533ebf00096ee47f605036a6f1
alireza <alirezahn1@yahoo.com>
2023-06-04 13:31:31 +0330

    fix index expense view

--- expenses/views.py
+++ expenses/views.py
@@ -1,7 +1,22 @@
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 from django.shortcuts import render
+
+from expenses.models import Expense, Category
+from userpreferences.models import UserPreference
 
 
 @login_required
 def index(request):
-    return render(request,'base.html')
+    categories = Category.objects.all()
+    expenses = Expense.objects.filter(owner=request.user)
+    paginator = Paginator(expenses, 5)
+    page_number = request.GET.get('page')
+    page_obj = Paginator.get_page(paginator, page_number)
+    currency = UserPreference.objects.get(user=request.user).currency
+    context = {
+        'expenses': expenses,
+        'page_obj': page_obj,
+        'currency': currency
+    }
+    return render(request, 'expenses/index.html', context)
