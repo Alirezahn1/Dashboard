333d1bf083076cd5609f6ccba799687a77fe954d
alireza <alirezahn1@yahoo.com>
2023-06-04 16:10:00 +0330

    feat index income view

--- incomes/views.py
+++ incomes/views.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 from django.shortcuts import render
 
-# Create your views here.
+from incomes.models import Source, Income
+from userpreferences.models import UserPreference
+
+
+@login_required
+def index(request):
+    categories = Source.objects.all()
+    income = Income.objects.filter(owner=request.user)
+    paginator = Paginator(income, 5)
+    page_number = request.GET.get('page')
+    page_obj = Paginator.get_page(paginator, page_number)
+    currency = UserPreference.objects.get(user=request.user).currency
+    context = {
+        'income': income,
+        'page_obj': page_obj,
+        'currency': currency
+    }
+    return render(request, 'income/index.html', context)
